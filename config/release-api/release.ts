1e39d424ef9ca16a0db3eea06a89b044b3aa4df3
Jonathan Guichard <jona.guich69@gmail.com>
2022-02-26 01:31:19 +0100

    All -- Feature -- Upgrade to electron 17

--- config/release-api/release.ts
+++ config/release-api/release.ts
@@ -1,23 +1,29 @@
 import { readdir, readFile, remove, rm, writeFile } from "fs-extra";
-import axios from "axios";
 import * as path from "path";
-import { spawnAsync } from "../app/src/main/util";
+import { spawnAsync } from "../../app/src/main/util";
+import { AppsApi, AppVersion } from "./api";
 
-const packageJson = path.join(__dirname, "..", "package.json");
-const appPackageJson = path.join(__dirname, "..", "app", "package.json");
+const appApi = new AppsApi(undefined, "http://localhost:4001");
+
+const packageJson = path.join(__dirname, "..", "..", "package.json");
+const appPackageJson = path.join(__dirname, "..", "..", "app", "package.json");
+
+async function updateVersion(version: string) {
+	const appJson = JSON.parse((await readFile(appPackageJson)).toString());
+	appJson.version = version;
+	await writeFile(appPackageJson, JSON.stringify(appJson, null, 4));
+}
 
 const main = async () => {
 	let pkg = JSON.parse((await readFile(packageJson)).toString());
 	let pkgApp = JSON.parse((await readFile(appPackageJson)).toString());
 	const originalPkg = { ...pkg };
 	let [major, minor, build] = pkg.version.split(".").map((str: string) => Number.parseInt(str));
+	const oldVersion = [major, minor, build].join(".");
 	minor += 1;
 	let version = [major, minor, build].join(".");
 	pkg = { ...pkg, version };
-	const updatePackageJson = (pkg: any) => {
-		writeFile(appPackageJson, JSON.stringify({ ...pkg }, undefined, 4));
-		writeFile(packageJson, JSON.stringify({ ...pkg }, undefined, 4));
-	};
+	const updatePackageJson = (pkg: any) => writeFile(packageJson, JSON.stringify({ ...pkg }, undefined, 4));
 
 	const nodeBinaries = path.resolve(path.dirname(packageJson), "node_modules", ".bin");
 
@@ -28,13 +34,13 @@ const main = async () => {
 	try {
 		// await copy(path.join(__dirname, "..", "src"), path.join(__dirname, "..", "build-config"));
 
-		await updatePackageJson(pkg);
+		await updateVersion(version);
 		await spawnAsync(`yarn build && ${electronBuilderBin} -w --publish never`, {
 			cwd: path.dirname(packageJson),
 			ignoreErrors: true,
 			color: true,
 		});
-		await updatePackageJson(originalPkg);
+		await updateVersion(oldVersion);
 
 		const outputFolder = path.resolve(path.dirname(packageJson), pkg.build.directories.output);
 		const files = await readdir(outputFolder);
@@ -46,20 +52,19 @@ const main = async () => {
 
 		const installerData = await readFile(path.join(outputFolder, installerPath));
 		await Promise.all([
-			send(version, [...installerData], "windows"),
+			send({ major, minor, revision: build }, [...installerData], "windows"),
 			// send(version, [...installerData], "linux")
 		]);
 
 		await spawnAsync(`git tag v${version} && git push --tags`);
 
-		await updatePackageJson(pkg);
-
-		await remove(path.join(__dirname, "..", "dist"));
+		await updateVersion(version);
+		await remove(path.join(__dirname, "..", "..", "app", "dist"));
 	} catch (e: any) {
 		console.error("ERROR", e.message);
 	} finally {
 		await Promise.all([
-			rm(path.join(__dirname, "..", "dist"), {
+			rm(path.join(__dirname, "..", "..", "app", "dist"), {
 				force: true,
 				recursive: true,
 			}),
@@ -71,29 +76,26 @@ const main = async () => {
 
 main();
 
-async function send(version: string, data: number[], platform: "windows" | "linux") {
+async function send(version: AppVersion, data: number[], platform: "windows" | "linux") {
 	return new Promise<void>(async resolve => {
 		try {
 			console.log("Uploading file to server");
 
-			const call = await axios.post(
-				"https://elyspio.fr/updater/api/media-tools/windows",
+			await appApi.add(
 				{
-					data: data,
-					version,
+					binary: data,
+					metadata: {
+						version,
+						arch: platform === "windows" ? "Win64" : "Linux64",
+						name: "Elytools",
+					},
 				},
 				{
+					onUploadProgress: (...e) => console.log(...e),
 					maxBodyLength: data.length * 10,
-					maxContentLength: data.length * 10,
-					onUploadProgress: (...e) => console.log(e),
 				}
 			);
 
-			console.log(call.status);
-
-			if (call.status !== 200) {
-				throw new Error(`${call.status}`);
-			}
 			resolve();
 
 			console.log("sent to server");
@@ -107,4 +109,3 @@ async function send(version: string, data: number[], platform: "windows" | "linu
 	});
 }
 
-// "release": "npm run build-config && util-builder -w"
