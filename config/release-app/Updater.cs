73b1a28be8d4d0afab266836f77117ea4a19d764
Jonathan Guichard <jona.guich69@gmail.com>
2022-03-04 00:57:20 +0100

    Deploy -- Feature --- Retry upload on error

--- config/release-app/Updater.cs
+++ config/release-app/Updater.cs
@@ -6,12 +6,23 @@ public class UpdaterApi
 {
     public static async Task Upload(string path, AppVersion version, AppArch arch)
     {
-        var t = DateTime.Now;
-        var api = new AppsClient { BaseUrl = "http://localhost:4000" };
-        var bytes = File.ReadAllBytes(path);
-        Console.WriteLine("Adding " + path);
-        await api.AddAsync("Elytools", version.ToString(), arch, bytes);
-        Console.WriteLine($"Added {path} in {(DateTime.Now - t).Seconds}s");
+        try
+        {
+            var t = DateTime.Now;
+            var api = new AppsClient { BaseUrl = "https://elyspio.fr/updater" };
+            var bytes = File.ReadAllBytes(path);
+            Console.WriteLine("Adding " + path);
+            await api.AddAsync("Elytools", version.ToString(), arch, bytes);
+            Console.WriteLine($"Added {path} in {(DateTime.Now - t).Seconds}s");
+        }
+
+        catch(Exception e)
+        {
+            Console.WriteLine($"Error in upload of {path}, retry in 1s");
+            await Task.Delay(1000);
+            await Upload(path, version, arch);
+        }
+
 
     }
 }
